fccafd67bc8e2097edb51c362961c5f8de82a414
DineshNehra29 <156710996+DineshNehra29@users.noreply.github.com>
2024-03-14 03:48:19 +0530

    added and changed a lot

--- review.js
+++ review.js
@@ -1,11 +1,11 @@
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper(".as", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
     centerSlide: 'true',
+    
     fade: 'true',
     grabCursor: 'true',
-   
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
@@ -18,13 +18,13 @@ var swiper = new Swiper(".slide-content", {
 
     breakpoints:{
         0: {
-            slidesPerView: 2,
+            slidesPerView: 1,
         },
         520: {
-            slidesPerView: 3,
+            slidesPerView: 1,
         },
         950: {
-            slidesPerView: 4,
+            slidesPerView: 2,
         },
     },
   });
